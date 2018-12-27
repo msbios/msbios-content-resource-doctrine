c468c956ef71853cb854728f85cedf41238e4cb6
Judzhin Miles <info@msbios.com>
2018-12-27 11:58:24 +0200

    Lazy Composer MSBios Commit Script

---
+++ src/Form/TestPageForm.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @access protected
+ * @author Judzhin Miles <info[woof-woof]msbios.com>
+ */
+namespace MSBios\Content\Resource\Doctrine\Form;
+
+use MSBios\Content\Resource\Form\TextPageForm as DefaultTextPageForm;
+use MSBios\Doctrine\Form\Element\PublishingState;
+
+/**
+ * Class TestPageForm
+ * @package MSBios\Content\Resource\Doctrine\Form
+ */
+class TestPageForm extends DefaultTextPageForm
+{
+    /**
+     * @inheritdoc
+     *
+     * @return $this|DefaultTextPageForm|void
+     */
+    public function init()
+    {
+        parent::init();
+
+        $this->add([
+            'type' => PublishingState::class,
+            'name' => 'state'
+        ]);
+
+        return $this;
+    }
+}
--- src/Module.php
+++ src/Module.php
@@ -13,7 +13,7 @@ namespace MSBios\Content\Resource\Doctrine;
 class Module extends \MSBios\Module
 {
     /** @const VERSION */
-    const VERSION = '1.0.3';
+    const VERSION = '1.0.4';
 
     /**
      * @inheritdoc
