a981b28b5d090dceefed5a44db59b7c9cb0ce875
Judzhin Miles <info@msbios.com>
2019-05-27 16:32:20 +0300

    Lazy Composer MSBios Commit Script

--- src/Module.php
+++ src/Module.php
@@ -13,7 +13,7 @@ namespace MSBios\Content\Resource\Doctrine;
 class Module extends \MSBios\Module
 {
     /** @const VERSION */
-    const VERSION = '1.0.6';
+    const VERSION = '1.0.7';
 
     /**
      * @inheritdoc
